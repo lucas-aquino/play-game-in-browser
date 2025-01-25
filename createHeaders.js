f7d881f077723536f3e7407134ba5616fc6e045a
lucas-aquino <luckasakino@protonmail.com>
2025-01-24 21:10:56 -0300

    Modify _header

---
+++ createHeaders.js
@@ -0,0 +1,15 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const headersContent = `
+/src/juego/Build_v13/Build/Build_v13.loader.js
+Content-Type: application/javascript
+`;
+
+fs.outputFile(path.join(__dirname, 'dist', '_headers'), headersContent, (err) => {
+  if (err) {
+    console.error('Error creating _headers file', err);
+  } else {
+    console.log('_headers file created successfully in the dist folder');
+  }
+});
