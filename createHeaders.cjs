0bdc159e2f926bd1bf283ea9ee4bef302ef2264a
lucas-aquino <luckasakino@protonmail.com>
2025-01-24 21:59:32 -0300

    Modify _header

--- createHeaders.cjs
+++ createHeaders.cjs
@@ -2,13 +2,8 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const headersContent = `
-
 /src/juego/Build_v13/Build/Build_v13.loader.js
-  Content-Type: application/javascript
-
-/src/juego/Build_v13/Build/Build_v13.framework.js
-  Content-Type: application/javascript
-
+Content-Type: application/javascript
 `;
 
 fs.outputFile(path.join(__dirname, 'dist', '_headers'), headersContent, (err) => {
