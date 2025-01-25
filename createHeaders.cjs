07ecf3790151360347dff6080a6489de5c20957f
lucas-aquino <luckasakino@protonmail.com>
2025-01-24 21:15:23 -0300

    Modify _header

--- createHeaders.cjs
+++ createHeaders.cjs
@@ -3,7 +3,7 @@ const path = require('path');
 
 const headersContent = `
 /src/juego/Build_v13/Build/Build_v13.loader.js
-Content-Type: application/javascript
+  Content-Type: application/javascript
 `;
 
 fs.outputFile(path.join(__dirname, 'dist', '_headers'), headersContent, (err) => {
